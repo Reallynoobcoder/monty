c1dbfd93cd56d58bc867c637855d9714aa6ee8a7
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-20 06:52:08 -0700

    back to struct

--- main.c
+++ main.c
@@ -4,162 +4,158 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 char *line = NULL;
 char **tokens = NULL;
-
-int isnumeric(const char *str)
-{
-    int i;
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (!isdigit(str[i]))
-            return (0);
-    }
-    return (1);
-}
-
 void free_dlistint(stack_t *head)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    while (head)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
-
 void push(stack_t **stack, unsigned int line_number)
 {
-    char *arg = tokens[1];
-    int value;
-    stack_t *new_node;
+	char *arg = tokens[1];
+	int value, i;
+	stack_t *new_node;
 
-    if (!arg || !isnumeric(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        free(line);
-        exit(EXIT_FAILURE);
-    }
+	if (!arg)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		free(line);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; arg[i] != '\0'; i++)
+	{
+		if (!isdigit(arg[i]))
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+	}
+	value = atoi(arg);
 
-    value = atoi(arg);
+	new_node = malloc(sizeof(stack_t));
 
-    new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(line);
+		exit(EXIT_FAILURE);
+	}
 
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        free(line);
-        exit(EXIT_FAILURE);
-    }
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	if (*stack)
+	{
+		(*stack)->prev = new_node;
+	}
 
-    if (*stack)
-    {
-        (*stack)->prev = new_node;
-    }
-
-    *stack = new_node;
+	*stack = new_node;
 }
-
 void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *curr;
     (void)line_number;
+	
 
-    if (!stack || !*stack)
-        return;
+	if (stack == NULL)
+		return;
 
-    curr = *stack;
+	curr = *stack;
 
-    while (curr)
-    {
-        printf("%d\n", curr->n);
-        curr = curr->next;
-    }
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+	}
 }
 
 char **split(char *str)
 {
-    char *token = strtok(str, " \t\n\r\f");
-    char **array = malloc(sizeof(*array) * 1024);
-    size_t i = 0;
+	char *token = strtok(str, " \t\n\r\f");
+	char **array = malloc(sizeof(*array) * 1024);
+	size_t i = 0;
 
-    for (i = 0; token != NULL; i++)
-    {
-        array[i] = token;
-        token = strtok(NULL, " \t\n\r\f");
-    }
-    array[i] = NULL;
-    return (array);
+	for (i = 0; token != NULL; i++)
+	{
+		array[i] = token;
+		token = strtok(NULL, " \t\n\r\f");
+	}
+	array[i] = NULL;
+	return (array);
 }
 
 int main(int ac, char **av)
 {
-    size_t size = 0;
-    unsigned int line_number = 1;
-    FILE *fp;
-    int found = 0;
-    stack_t *stack = NULL;
+	size_t size = 0;
+	unsigned int line_number = 1;
+	FILE *fp;
+    int found = 0, i;
+	stack_t *stack = NULL;
 
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    fp = fopen(av[1], "r");
+	fp = fopen(av[1], "r");
 
-    if (fp == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &size, fp) != -1)
+	{
+		tokens = split(line);
+		if (tokens[0] == NULL)
+		{
+			free(tokens);
+			continue;
+		}
+	
+		for (i = 0; opcodes_Fun[i].opcode != NULL; i++)
+		{
+		    if (strcmp(tokens[0], opcodes_Fun[i].opcode) == 0)
+		    {
+		        opcodes_Fun[i].f(&stack, line_number);
+                found = 1;
+                break;
+		    }
+		}
+	/*	if (strcmp(tokens[0], "push") == 0)
+		{
+			push(&stack, line_number);
 
-    while (getline(&line, &size, fp) != -1)
-    {
-        tokens = split(line);
-        if (tokens[0] == NULL)
-        {
-            free(tokens);
-            continue;
-        }
+		}
+		else if (strcmp(tokens[0], "pall") == 0)
+		{
+			pall(&stack, line_number);
+		}*/
+		
+		if (!found)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, tokens[0]);
+			free(tokens);
+			free_dlistint(stack);
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		free(tokens);
+		line_number++;
 
-        found = 0;
-
-        if (strcmp(tokens[0], "push") == 0)
-        {
-            push(&stack, line_number);
-            found = 1;
-        }
-        else if (strcmp(tokens[0], "pall") == 0)
-        {
-            pall(&stack, line_number);
-            found = 1;
-        }
-
-        if (!found)
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, tokens[0]);
-            free(tokens);
-            free_dlistint(stack);
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-
-        free(tokens);
-        line_number++;
-    }
-
-    free_dlistint(stack);
-    free(line);
-    fclose(fp);
-    return (0);
+	}
+	free_dlistint(stack);
+	free(line);
+	fclose(fp);
+	return (0);
 }
-
