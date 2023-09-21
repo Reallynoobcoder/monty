578c19982ba2585a9eb413981061aeb27394f227
Elimali hamza <elimal.hamza@gmail.com>
2023-09-21 05:26:50 -0700

    calcul

--- calcul.c
+++ calcul.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
@@ -10,13 +16,20 @@ void add(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
-	
+
 	temp->next->n = temp->next->n + temp->n;
 	pop(stack, line_number);
 }
+
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
 void sub(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
 	if (!*stack || !(*stack)->next)
 	{
@@ -24,13 +37,20 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
-	
+
 	temp->next->n = temp->next->n - temp->n;
 	pop(stack, line_number);
 }
+
+/**
+ * divide - Divides the second top element of the stack by the top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
 void divide(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
 	if (!*stack || !(*stack)->next)
 	{
@@ -40,15 +60,22 @@ void divide(stack_t **stack, unsigned int line_number)
 	temp = *stack;
 	if (temp->n == 0)
 	{
-	    fprintf(stderr, "L%d: division by zero\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp->next->n = temp->next->n / temp->n;
 	pop(stack, line_number);
 }
+
+/**
+ * mul - Multiplies the second top element of the stack by the top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
 void mul(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
 	if (!*stack || !(*stack)->next)
 	{
@@ -56,13 +83,21 @@ void mul(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
-	
+
 	temp->next->n = temp->next->n * temp->n;
 	pop(stack, line_number);
 }
+
+/**
+ * mod - Computes the remainder of the division of the second top element
+ *       of the stack by the top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ */
+
 void mod(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
 	if (!*stack || !(*stack)->next)
 	{
@@ -72,7 +107,7 @@ void mod(stack_t **stack, unsigned int line_number)
 	temp = *stack;
 	if (temp->n == 0)
 	{
-	    fprintf(stderr, "L%d: division by zero\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp->next->n = temp->next->n % temp->n;
