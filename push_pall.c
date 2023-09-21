02711deb7b4dbdcd757fe781e215283dcd030403
Elimali hamza <elimal.hamza@gmail.com>
2023-09-21 12:11:41 -0700

    a

--- push_pall.c
+++ push_pall.c
@@ -60,7 +60,6 @@ void push(stack_t **stack, unsigned int line_number)
  * @stack: Double pointer to the beginning of the stack.
  * @line_number: The line number in the Monty bytecode file.
  */
-
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *curr;
