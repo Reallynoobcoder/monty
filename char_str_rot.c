5236a34a42e2a67e3f32589ebb328bf7009ea82d
Elimali hamza <elimal.hamza@gmail.com>
2023-09-22 03:13:06 -0700

    .

--- char_str_rot.c
+++ char_str_rot.c
@@ -84,3 +84,28 @@ void rotr(stack_t **stack, unsigned int line_number)
 	new_h->prev = NULL;
 	*stack = new_h;
 }
+
+/**
+ * pchar - Prints the character at the top of the stack as a character.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the Monty file where pchar was called.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n > -1 && (*stack)->n < 128)
+	{
+		putchar((*stack)->n);
+		putchar(10);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
